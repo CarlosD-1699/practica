2ecad4a95a1848944280abfab452ab20b6121dca
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-09-08 18:01:19 -0500

    feat: added validation rules to form3 inputs

--- src/app/formulario3/formulario3.component.ts
+++ src/app/formulario3/formulario3.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario3',
@@ -7,10 +12,32 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./formulario3.component.css'],
 })
 export class Formulario3Component {
-  name = new FormControl('', Validators.required);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  constructor(private fb: FormBuilder) {}
 
-  constructor() {}
+  get name() {
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  }
+
+  // formUser = new FormGroup({
+  //   name: new FormControl('', Validators.required),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  // });
+
+  formUser = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  });
+
+  procesar() {
+    console.log(this.formUser.value);
+  }
+
+  // name = new FormControl('', Validators.required);
+  // email = new FormControl('', [Validators.required, Validators.email]);
 
   ngOnInit(): void {}
 }
