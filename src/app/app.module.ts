dbb1d1d9e44a5081238385a5986a13514932cd41
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-09-13 15:08:37 -0500

    feat: implemented services to manage information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { SwitchComponent } from './switch/switch.component';
 import { Formulario2Component } from './formulario2/formulario2.component';
 import { Formulario3Component } from './formulario3/formulario3.component';
 import { ChildrenComponent } from './children/children.component';
+import { AddMessagesComponent } from './add-messages/add-messages.component';
+import { ListMessagesComponent } from './list-messages/list-messages.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,8 @@ import { ChildrenComponent } from './children/children.component';
     Formulario2Component,
     Formulario3Component,
     ChildrenComponent,
+    AddMessagesComponent,
+    ListMessagesComponent,
   ],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
